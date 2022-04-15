4764ff2d9f1ddff556636f502cac72e5f4150dca
yangyang876 <70964076+yangyang876@users.noreply.github.com>
2022-04-15 20:42:15 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -14,7 +14,7 @@ module.exports = {
   icon: '/icons/3434.png',
 
   // The name of your website. Present alongside your icon.
-  title: "Spencer's OneDrive",
+  title: "family's OneDrive",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/Public',
